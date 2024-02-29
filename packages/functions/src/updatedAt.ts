1434bd9a1b914c5ac1e8b024d3a66663a18dd47d
Herminio Garcia <hermano360@gmail.com>
2024-02-28 22:36:38 -0800

    Add updatedAt to get the latest time

--- packages/functions/src/updatedAt.ts
+++ packages/functions/src/updatedAt.ts
@@ -1,16 +1,45 @@
 import { ApiHandler } from "sst/node/api";
 import AWS from "aws-sdk";
-import * as uuid from "uuid";
+// import * as uuid from "uuid";
 // import { ApiGatewayProxyEventV2 } from "aws-lambda";
 import { Table } from "sst/node/table";
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-export const getLatest = ApiHandler(async (evt) => {
-  return {
-    statusCode: 200,
-    body: evt.requestContext.time,
+export const getLatest = ApiHandler(async () => {
+  const params = {
+    // Get the table name from the environment variable
+    TableName: Table.GolfChecker.tableName,
+    // Get all the rows where the userId is our hardcoded user id
+    KeyConditionExpression: "PK = :updatedAt",
+    ExpressionAttributeValues: {
+      ":updatedAt": "updatedAt",
+    },
+    Limit: 1,
+    ProjectionExpression: "updatedAt",
+    ScanIndexForward: false,
   };
+
+  try {
+    const results = await dynamoDb.query(params).promise();
+
+    const item = results.Items ? results.Items[0] : {};
+    return {
+      statusCode: 200,
+      body: JSON.stringify(item),
+    };
+  } catch (error) {
+    let message;
+    if (error instanceof Error) {
+      message = error.message;
+    } else {
+      message = String(error);
+    }
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: message }),
+    };
+  }
 });
 
 export const setLatest = ApiHandler(async (evt) => {
@@ -44,9 +73,4 @@ export const setLatest = ApiHandler(async (evt) => {
       body: JSON.stringify({ error: message }),
     };
   }
-
-  return {
-    statusCode: 200,
-    body: evt.requestContext.time,
-  };
 });
